348d45c5095f98be0ec07297b07f9d0c6bf1b204
SpectreAscended <94819487+SpectreAscended@users.noreply.github.com>
2022-01-10 18:41:12 -0600

    initial upload!

---
+++ game.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const buttonColors = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
+let started = false;
+let highscore = 0;
+
+const playSound = function (name) {
+  const audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+};
+
+const animatePress = function (currentColor) {
+  $(`.${currentColor}`).addClass('pressed');
+  setTimeout(function () {
+    $(`.${currentColor}`).removeClass('pressed');
+  }, 100);
+};
+
+const nextSequence = function () {
+  let randomNumber = Math.trunc(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+  level++;
+  $('#level-title').text(`Level ${level}`);
+  userClickedPattern = [];
+};
+
+$('.btn').click(function (event) {
+  let userChosenColor = event.target.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  console.log(userClickedPattern);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keydown(function () {
+  if (!started) {
+    $('#level-title').text(`Level ${level}`);
+    started = true;
+    nextSequence();
+  }
+});
+
+const checkAnswer = function (currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log('success');
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log('wrong');
+    playSound('wrong');
+    $('body').addClass('game-over');
+    setTimeout(function () {
+      $('body').removeClass('game-over');
+    }, 200);
+    if (level > highscore) highscore = level;
+    $('#level-title').text(
+      'Game Over, Press Any Key to Restart.  Highscore:' + highscore
+    );
+    startOver();
+  }
+};
+
+const startOver = function () {
+  level = 0;
+  gamePattern = [];
+  started = false;
+};
